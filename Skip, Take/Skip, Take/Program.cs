5cd61f42517efaf6c6055f2895f14a2607b8588d
nguyễn mạnh <manharia123@gmail.com>
2024-04-17 18:08:04 +0700

    Skip and take

---
+++ Skip, Take/Skip, Take/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Skip__Take
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>() { 2, 6, 4, 8, 3, 7, 9, 1, 10 };
+            var a = list.Take(2);
+            var b= list.Skip(3);
+            foreach(var x in a)
+            {
+                Console.Write(x+" ");
+            }
+            Console.WriteLine();
+            foreach (var x1 in b)
+            {
+                Console.Write(x1+" ");
+            }
+           
+            Console.ReadLine();
+        }
+    }
+}
